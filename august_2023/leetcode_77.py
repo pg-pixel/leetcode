04365535946ba7ed70658c67e5c7323160f3e819
pg <gautam.priyansh98@gmail.com>
2023-08-01 22:45:47 +0530

    daily leetcode 1st august 2023

---
+++ august_2023/leetcode_77.py
@@ -0,0 +1,10 @@
+''' 
+level=medium
+topic= backtarcking, but i used itertools
+
+Later I will implement it with backtracking
+''' 
+from itertools import combinations
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return list(combinations([i for i in range(1, n+1)], k))
